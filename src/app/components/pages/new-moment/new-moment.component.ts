3b9486289eb3fe51147744ea71d652fd0fbe41c4
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-04-15 23:46:24 -0300

    Projeto em andamento

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+import { Moment } from 'src/app/Moments';
+
+import { MomentService } from 'src/app/services/moment.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.scss'],
+})
+export class NewMomentComponent {
+  btnText = 'Compartilhar!';
+  image?: File;
+
+  constructor(private momentService: MomentService) {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+    t;
+
+    await this.momentService.createMoment(formData).subscribe();
+  }
+}
